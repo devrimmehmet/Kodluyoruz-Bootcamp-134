0bf5894a493f92582aa5652a8ad0d3457a1732f0
devrimmehmet <devrimmehmet@gmail.com>
2022-03-20 14:29:29 +0300

    çarpım tablosu uygulaması yapıldı

---
+++ Intro2/LoopInsideLoop/LoopInsideLoop/LoopInsideLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopInsideLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Çarpım Tablosu yazdırma
+             */
+
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i*j}");
+                }
+                Console.WriteLine("**************************");
+            }
+        }
+    }
+}
