ca295cf790862bbe730013c339d38879661d7a18
devrimmehmet <devrimmehmet@gmail.com>
2022-03-20 13:29:09 +0300

    minumum sayı ve maksimum sayı bulma

--- Intro2/Forloops/forLoop/forLoop/Program.cs
+++ Intro2/Forloops/forLoop/forLoop/Program.cs
@@ -30,14 +30,45 @@ namespace forLoop
             //Console.WriteLine("****************************************************************");
             //Console.WriteLine("****************************************************************");
 
-            int[] ages = { 42, 29, 26, 29, 29, 23 };
-            int ageTotal = 0;
-            for (int age = 0; age < ages.Length; age++)
-            {
-                ageTotal=ageTotal+ages[age];
+            //int[] ages = { 42, 29, 26, 29, 29, 23 };
+            //int ageTotal = 0;
+            //for (int age = 0; age < ages.Length; age++)
+            //{
+            //    //ageTotal=ageTotal+ages[age];
+            //    ageTotal+=ages[age];
                 
+            //}
+            //Console.WriteLine($"Yaşların toplamı :{ageTotal}");
+            //Console.WriteLine($"Yaşların Ortalaması :{ageTotal/ages.Length}");
+
+            int[] numbers = { 36, 12, 26, 9, -4, 22, 8 };
+            int minNumber = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+            {
+                if (minNumber > numbers[i])
+                {
+                    minNumber = numbers[i];
+                }
+                else
+                { }
             }
-            Console.WriteLine(ageTotal);
+            Console.WriteLine($"En Küçük Sayı : {minNumber}");
+            int maxNumber = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (maxNumber < numbers[i])
+                {
+                    maxNumber = numbers[i];
+                }
+                else
+                { }
+            }
+            Console.WriteLine($"En Büyük Sayı : {maxNumber}");
+
+
+
+
+
         }
     }
 }
