dea3e9c06e5518edb15b489eb05e22d3e6b0a14a
devrimmehmet <devrimmehmet@gmail.com>
2022-03-20 11:54:01 +0300

    Array and Generic List Explained

--- Intro2/Arrays/ArrayAndColections/ArrayAndColections/Program.cs
+++ Intro2/Arrays/ArrayAndColections/ArrayAndColections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArrayAndColections
 {
@@ -25,14 +26,38 @@ namespace ArrayAndColections
             // 42
             // kırk iki
 
-            string[] birler = new string[] { "", "Bir", "İki", "Üç","Dört","Beş","Altı","Yedi","Sekiz","Dokuz"};
-            string[] onlar = new string[] { "","On", "Yirmi", "Otuz","Kırk","Elli","Altmış","Yetmiş","Seksen","Doksan" };
-            int sayi=Convert.ToInt32(Console.ReadLine());
-            int birlerbas = sayi % 10;
-            int onlarbas = sayi / 10;
-            Console.WriteLine(birlerbas);
-            Console.WriteLine(onlarbas);
-            Console.WriteLine($"{onlar[onlarbas] } {birler[birlerbas]}");
+            // string[] birler = new string[] { "", "Bir", "İki", "Üç","Dört","Beş","Altı","Yedi","Sekiz","Dokuz"};
+            // string[] onlar = new string[] { "","On", "Yirmi", "Otuz","Kırk","Elli","Altmış","Yetmiş","Seksen","Doksan" };
+            // int sayi=Convert.ToInt32(Console.ReadLine());
+            // int birlerbas = sayi % 10;
+            // int onlarbas = sayi / 10;
+            //// Console.WriteLine(birlerbas);
+            //// Console.WriteLine(onlarbas);
+            // Console.WriteLine($"{onlar[onlarbas] } {birler[birlerbas]}");
+            //ödev bütün sayıları okuyan bir algoritma 16.697.885
+
+
+
+            //koleksiyona neden ihtiyaç duyarız ? Aşağıdaki kodu oku... ALTERNATİF GENERİC LİSTTİR...
+            //string[] shoppingList = new string[] { "Elma", "Salatalık", "Domates" };
+            //Array.Resize(ref shoppingList, shoppingList.Length + 1);
+            //shoppingList[shoppingList.Length-1]="Peynir";
+
+            //foreach(var item in shoppingList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            List<string> anotherShopList=new List<string>();
+            anotherShopList.Add("Peynir");
+            anotherShopList.Add("Zeytin");
+            anotherShopList.Add("Su");
+            foreach (var item in anotherShopList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //birçok list türü var generic dictionary firsinfirstout vb...
         }
     }
 }
