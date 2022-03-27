6fa5b76f1006b720bc45ef311510be04a3cea711
devrimmehmet <devrimmehmet@gmail.com>
2022-03-27 12:05:29 +0300

    son 5 dk eksik güncellenecek.

---
+++ Week-2/intro2/MethodDetails/MethodDetails/MethodDetails/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = divide(12, 3);
+            Console.WriteLine(result);
+            int outputValue = 0;
+            int result2 = divide(15, 2, out outputValue);
+            Console.WriteLine($"Bölüm: {result2}, kalan: {outputValue}");
+            int number = 8;
+            sample(number);
+            Console.WriteLine($"main metodundaki number değeri:{number}");
+          
+        }
+        static int divide(int a, int b)
+        {
+            return a / b;
+        }
+        static int divide(int a, int b,out int modulo)
+        {
+            modulo = a%b;
+            return a / b;
+        }
+        static void sample(int parameter)
+        {
+            parameter *=3;
+            Console.WriteLine(parameter);
+        }
+        
+    }
+}
