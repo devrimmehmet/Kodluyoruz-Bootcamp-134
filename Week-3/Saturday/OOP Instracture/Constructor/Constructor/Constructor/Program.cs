bba3b5990af6160259af9a7d6abbf963575c6371
devrimmehmet <devrimmehmet@gmail.com>
2022-04-09 12:12:03 +0300

    constructor example.

---
+++ Week-3/Saturday/OOP Instracture/Constructor/Constructor/Constructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Report report= new Report(); //parametre yok
+                                         //  report.CreatedDate= DateTime.Now;
+            Console.WriteLine($"Tarih: {report.CreatedDate}, Format: {report.Format}, ve Türü: {report.Type}");
+            
+            Report performanceReport= new Report("Devrim Mehmet");
+            Console.WriteLine($"Tarih: {performanceReport.CreatedDate}, Format: {performanceReport.Format}, ve Türü: {performanceReport.Type}, Bu Raporun Sahibi: {performanceReport.Owner}");
+
+            Report customReport =new Report(Formats.Excel);
+
+
+        }
+    }
+}
---
+++ Week-3/Saturday/OOP Instracture/Constructor/Constructor/Constructor/Report.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public enum Formats 
+    {
+    PDF,Excel
+    }
+    public class Report
+    {
+        public string Owner { get; set; }
+        public string Type { get; set; }
+        public Formats  Format { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+       
+        public Report() //amacı instance almak void değil geriye değerde döndürmez 
+        {
+            CreatedDate = DateTime.Now;
+            Format = Formats.PDF;
+            Type = "Monthly Sales Report";
+        }
+
+        public Report(string owner):this()//constructor constructoru this ile çağırır.
+        {
+            Owner = owner;
+        }
+
+        public Report(Formats format):this()
+        {
+            Format = format;
+        }
+    }
+}
